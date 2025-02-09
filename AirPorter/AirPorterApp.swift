7b131e178a06412f5264d027fbc0fc246627a7f8
Aniruddha Shukla <aniruddha.shukla@hotmail.com>
2025-02-09 10:14:26 -0600

    Initial Commit

---
+++ AirPorter/AirPorterApp.swift
@@ -0,0 +1,17 @@
+//
+//  AirPorterApp.swift
+//  AirPorter
+//
+//  Created by Aniruddha Shukla on 2/9/25.
+//
+
+import SwiftUI
+
+@main
+struct AirPorterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
